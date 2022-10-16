fec5794b80902ec04af5ce847d80a4d403bd1876
TIRAJET W <25123311+tirajetw@users.noreply.github.com>
2022-10-16 13:43:29 +0700

    Update decentralBank.tests.js

--- test/decentralBank.tests.js
+++ test/decentralBank.tests.js
@@ -5,41 +5,40 @@ const DecentralBank = artifacts.require("DecentralBank");
 /* eslint-disable no-undef */
 
 require("chai")
-    .use(require("chai-as-promised"))
-    .should();
+  .use(require("chai-as-promised"))
+  .should();
 
-contract("decentralBank", ({owner, customer}) => {
-    let rwd, tether;
+contract("decentralBank", ({ owner, customer }) => {
+  let rwd, tether, decentralBank;
 
-    function tokens(number) {
-        return web3.utils.toWei(number, "ether");
-    }
+  function tokens(number) {
+    return web3.utils.toWei(number, "ether");
+  }
 
-    before(async () => {
-        // Load Contracts
-        tether = await Tether.new();
-        rwd = await RWD.new();
-        decentralBank = await DecentralBank.new(rwd.address, tether.address);
-    
-        // Transfer all RWD tokens to DecentralBank (1 million)
-        await rwd.transfer(decentralBank.address, web3.utils.toWei("1000000"));
+  before(async () => {
+    // Load Contracts
+    tether = await Tether.new();
+    rwd = await RWD.new();
+    decentralBank = await DecentralBank.new(rwd.address, tether.address);
 
-        // Transfer 100 Mock Tether tokens to investor
-        await tether.transfer(customer, web3.utils.toWei("100"), { from: owner });
-    })
+    // Transfer all RWD tokens to DecentralBank (1 million)
+    await rwd.transfer(decentralBank.address, web3.utils.toWei("1000000"));
 
-    describe("Mock Tether Deployment", async () => {
-        it("matches name successfully", async () => {
-            const name = await tether.name();
-            assert.equal(name, "Mock Tether Token");
-        })
-    })
+    // Transfer 100 Mock Tether tokens to investor
+    await tether.transfer(customer, web3.utils.toWei("100"), { from: owner });
+  });
 
-    describe("RWD Deployment", async () => {
-        it("matches name successfully", async () => {
-            const name = await rwd.name();
-            assert.equal(name, "Reward Token");
-        })
-    })
+  describe("Mock Tether Deployment", async () => {
+    it("matches name successfully", async () => {
+      const name = await tether.name();
+      assert.equal(name, "Mock Tether Token");
+    });
+  });
 
+  describe("RWD Deployment", async () => {
+    it("matches name successfully", async () => {
+      const name = await rwd.name();
+      assert.equal(name, "Reward Token");
+    });
+  });
 });
